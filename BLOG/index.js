c36241e54ceddca81a1967531ea1ff61f077dc1a
Ashish Rana <ashishrana288@gmail.com>
2026-08-29 17:32:24 +0530

    Authentication complete

--- BLOG/index.js
+++ BLOG/index.js
@@ -3,12 +3,8 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cookiePaser = require('cookie-parser');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
- 
 const app = express();
-
-
 const userRoute = require('./routes/user');
-
 const PORT = 8000;
 
 mongoose.connect('mongodb://localhost:27017/blogu').then(e => console.log('MongoDB Connected'));
@@ -18,16 +14,13 @@ app.set("views",path.resolve("./views"));
 
 app.use(express.urlencoded({extended: false}));
 app.use(cookiePaser());
-app.use(checkForAuthenticationCookie('token'));
-
+app.use(checkForAuthenticationCookie("token"));
 
 app.get("/", (req, res) => {
     res.render("home", {
         user: req.user,
     });
-   
 });
 
 app.use('/user', userRoute);
-
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
--- BLOG/middlewares/authentication.js
+++ BLOG/middlewares/authentication.js
@@ -1,28 +1,19 @@
+const { validateToken } = require("../services/authentication");
+
 function checkForAuthenticationCookie(cookieName) {
-
     return (req, res, next) => {
-
         const tokenCookieValue = req.cookies[cookieName];
-
         if (!tokenCookieValue) {
             return next();
         }
-
         try {
-
             const userPayload = validateToken(tokenCookieValue);
-
             req.user = userPayload;
-
-        } catch (error) {
-
-            req.user = null;
-
-        }
-
+        } catch (error) {}
         return next();
     };
 }
+
 
 module.exports = {
     checkForAuthenticationCookie,
