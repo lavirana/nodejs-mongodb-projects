948061be70fd0122cba249e699b70df616b13add
Ashish Rana <ashishrana288@gmail.com>
2026-09-06 20:38:03 +0530

    websocket - Real time messaging completed

--- CHAT-APP/index.js
+++ CHAT-APP/index.js
@@ -9,8 +9,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 //Handle Socket.io
-io.on('connection', socket => {
-    console.log("A new user has connected", socket.id);
+io.on('connection', (socket) => {
+    socket.on('user-message', (message) => {
+        io.emit('message', message);
+    });
 });
 
 app.use(express.static(path.resolve('./public')));
