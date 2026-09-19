62909277ecbd9f272cd9433a69fcc890e96b31e1
Ashish Rana <ashishrana288@gmail.com>
2026-09-19 14:46:55 +0530

    added user routes code

---
+++ MYCONTACTS-BACKEND/controllers/contactController.js
@@ -0,0 +1,36 @@
+//@desc Get all the contacts
+//@routes GET  /api/contacts
+//@access public
+const  getContacts = (req, res) => {
+    res.status(200).json({message: "Get All Contacts"});
+};
+
+//@desc Create New contact
+//@routes POST  /api/contacts
+//@access public
+const  createContact = (req, res) => {
+    res.status(201).json({message: "Create Contact"});
+};
+
+//@desc Get contact
+//@routes GET  /api/contacts/:id
+//@access public
+const  getContact = (req, res) => {
+    res.status(200).json({message: `Get Contact for ${req.params.id}`});
+};
+
+//@desc Update contact
+//@routes PUT  /api/contacts/:id
+//@access public
+const  updateContact = (req, res) => {
+    res.status(200).json({message: `Update Contact for ${req.params.id}`});
+};
+
+//@desc Delete contact
+//@routes DELETE  /api/contacts/:id
+//@access public
+const  deleteContact = (req, res) => {
+    res.status(200).json({message: `Delete Contact for ${req.params.id}`});
+};
+
+module.exports = {getContacts, createContact, getContact, updateContact, deleteContact};
