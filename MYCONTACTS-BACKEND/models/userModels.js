9e37f4d711852e174dad47210773cb477481ccb2
Ashish Rana <ashishrana288@gmail.com>
2026-09-25 14:08:06 +0530

    added haspassword setup

---
+++ MYCONTACTS-BACKEND/models/userModels.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const usertSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the user name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the user email address"],
+        unique: [true, "Email address already taken"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please ass the user Password"],
+    }
+},{
+    timestamps: true,
+}
+);
+
+module.exports = mongoose.model("User", usertSchema);
